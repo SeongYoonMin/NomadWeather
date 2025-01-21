9fcb788b230ad3d20f6379dc6793bfc61a257d79
SeongYoon <namo4862@gmail.com>
2025-01-21 16:26:19 +0900

    fix: 기본디자인

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,63 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
+
+const deviceWidth = Dimensions.get("window").width;
 
 export default function Index() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Text>NEW APP</Text>
-        <StatusBar style="auto" />
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
       </View>
-    </SafeAreaView>
+      <ScrollView
+        contentContainerStyle={styles.weather}
+        horizontal
+        pagingEnabled
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+      </ScrollView>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#FFF",
+  },
+  city: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
     justifyContent: "center",
     alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {},
+  day: {
+    width: deviceWidth,
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 178,
+  },
+  desc: {
+    fontSize: 60,
   },
 });
