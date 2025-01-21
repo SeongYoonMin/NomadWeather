7c5d460107843c160ee247ebe7718fc11fcdeda3
SeongYoon <namo4862@gmail.com>
2025-01-21 14:12:37 +0900

    feat: FIRST COMMIT

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>NEW APP</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
